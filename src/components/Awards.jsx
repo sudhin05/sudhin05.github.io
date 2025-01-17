193288ae800ffa392c5c2d47bdd682549f518977
Sudhindra Pai <144920228+sudhin05@users.noreply.github.com>
2025-01-17 13:27:37 +0530

    Update Awards.jsx

--- src/components/Awards.jsx
+++ src/components/Awards.jsx
@@ -29,29 +29,19 @@ const Awards = () => {
                </div>
                </div>
                <div className='awards-item'>
-               <div style={{width: '40px'}}><FontAwesomeIcon icon={faUserGraduate} /></div>
-               <div>
-                  <span className='award-text-header'>JEE (Joint Entrance Examination) Mains 2023</span>
-                  <br />
-                  <span className='award-date'>May 2023</span>
-                  <br />
-                  Achieved an all-India rank of 8,836 with a 99.27 percentile in JEE Mains 2023 among a total of 1.1M students who appeared for the exam.
-               </div>
-               </div>
-               <div className='awards-item'>
                <div style={{width: '40px'}}><FontAwesomeIcon icon={faMedal} /></div>
                <div>
                   <span className='award-text-header'>Hanoi Open Mathematics Competition National Qualifiers</span>
                   <br />
                   <span className='award-date'>Jun 2021</span>
                   <br />
-                  Achiever an All-India Rank of 122 in the esteemed HOMC National qualifiers 2020-21, with over 10,000 contestants participating from 50 cities across the country
+                  All-India Rank of 122 in the esteemed HOMC National qualifiers 2020-21, with over 10,000 contestants participating from 50 cities across the country
                </div>
                </div>
                <div className='awards-item'>
                <div style={{width: '40px'}}><FontAwesomeIcon icon={faUserGraduate} /></div>
                <div>
-                  <span className='award-text-header'>School Topper - Central Board of Secondary Education (CBSE) Class 10th Board Examinations </span>
+                  <span className='award-text-header'> Central Board of Secondary Education (CBSE) Class 10th Board Examinations </span>
                   <br />
                   <span className='award-date'>May 2021</span>
                   <br />
